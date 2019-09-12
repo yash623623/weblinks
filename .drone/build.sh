8adb1d149554fb2ca0c09fa45dd89652d8897981
wilsonge <georgejameswilson@googlemail.com>
2019-09-12 11:56:51 +0100

    Customise the CMS directory so it's in the apache scope

--- .drone/build.sh
+++ .drone/build.sh
@@ -6,16 +6,14 @@ echo "Started building at $(date) - $(whoami)"
 # Update composer
 composer self-update
 
-# show directory listing
-ls -al
-ls -al vendor
-mount
-
 # Install dependencies
 composer install --no-interaction --no-progress
 
 cp jorobo.dist.ini jorobo.ini
 cp RoboFile.dist.ini RoboFile.ini
+
+# Make sure the path to the CMS is inside the apache config for docker
+sed -i 's!^cmsPath\s*=\s*(.*)!\/tests\/joomla/$1!' RoboFile.ini
 
 # Build package
 vendor/bin/robo build --dev
