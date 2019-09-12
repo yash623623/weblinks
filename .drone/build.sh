32b7e75867eaaba7bc961e39e4411a146b6d0c39
wilsonge <georgejameswilson@googlemail.com>
2019-09-12 13:50:52 +0100

    Temporary workaround

--- .drone/build.sh
+++ .drone/build.sh
@@ -13,7 +13,7 @@ cp jorobo.dist.ini jorobo.ini
 cp RoboFile.dist.ini RoboFile.ini
 
 # Make sure the path to the CMS is inside the apache config for docker
-sed -i 's!^cmsPath\s*=\s*(.*)!\/tests\/joomla/$1!' RoboFile.ini
+sed -i 's!tests/joomla!/tests/joomla!' RoboFile.ini
 
 # Build package
 vendor/bin/robo build --dev
